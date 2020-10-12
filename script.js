01776c1074bc77e7824efc415363889ce87663fd
Kevin S <alphalop42@gmail.com>
2020-10-11 23:45:18 -0700

    Update changes -Fixed up the css/html to remove awkward border gap -Built up the gameBoard module to where it can now populate the display

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const gameBoard = (() => {
-  let arrayGB = ["X", "O", "X", 
-                 "X", "O", "X", 
-                 "X", "O", "X"];
+    let arrayGB = [];
+    const gameElement = index => {
+        const gridID = document.getElementById('grid'+index);
+        let value = '';
+        return {gridID, value};
+    };
+    //populates the game elements
+    for(i = 0; i < 9; i++){
+        arrayGB[i] = gameElement(i);
+        if(i%2===0){arrayGB[i].value = 'X';}
+        else{arrayGB[i].value = 'O';}
+        arrayGB[i].gridID.textContent = arrayGB[i].value;
+    }
+    console.table(arrayGB);
 })();
