8a3038753bf0d3d2b8ebc79ed06cb2290b8d1925
Kevin S <alphalop42@gmail.com>
2020-10-20 21:14:08 -0700

    Incremental -Adding turn tracker to gameBoard -Adding extended functionality to enable entry of O's on the render method

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const gameBoard = (() => {
     const arrayGameElements = [];
+    //Internal value to keep track of the current turn without input from outside
+    let _lastValue = 'O';
     gameElement = index => {
         this.gridID = document.getElementById('grid'+index);
         //_bindEvents();
@@ -7,19 +9,26 @@ const gameBoard = (() => {
         this.value = '';
         return {gridID, value};
     };
-    const _cacheDom = index => {
-        //this.gridID = tihs.document.getElementById('grid'+index);
+    const _render =  e => {
+        if(e.target.textContent != ''){console.log("space already filled");}
+        else {
+            e.target.textContent = _lastValue === 'O'? 'X' : 'O';
+            _lastValue = e.target.textContent;
+            let temp = e.target.id.replace( /^\D+/g, '');
+            console.log(temp);
+            
+        }
     };
-    const _bindEvents = () => {};
-    const _render =  event => {
-        if(event.target.textContent != ''){console.log("space already filled");}
-        else {event.target.textContent = 'X';}
-    };
-
     const  _populateArray = () => {for(i = 0; i < 9; i++) {arrayGameElements[i] = gameElement(i);} };
     const getGameElement = index => arrayGameElements[index];
+    const resetBoard = () => {};
     _populateArray();
     console.table(arrayGameElements);
+    //Reserved methods:
+    const _cacheDom = index => {};
+    const _bindEvents = () => {};
+
+
 })();
 
 const player = (isFirstPlayer) => {
